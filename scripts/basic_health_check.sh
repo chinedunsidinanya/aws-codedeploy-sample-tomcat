74692894a04b399c7cab77e09f0e63001bc361d5
Chinedu Nsidinanya <chineducarson@icloud.com>
2021-06-12 10:00:54 -0500

    redeploying my application

--- scripts/basic_health_check.sh
+++ scripts/basic_health_check.sh
@@ -7,7 +7,7 @@ do
     echo "Successfully pulled root page."
     exit 0;
   fi
-  echo "Attempt to curl endpoint returned HTTP Code $HTTP_CODE. Backing off and retrying."
+  echo "Attempt to curl endpoint returned HTTP Code $HTTP_CODE. Backing off and retrying.."
   sleep 10
 done
 echo "Server did not come up after expected time. Failing."
